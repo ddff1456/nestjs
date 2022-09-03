a60e60ac14ad729ee8fcbda569ad5a2dcd1526d7
kyeongikLee <ddff1456@naver.com>
2022-09-04 01:14:58 +0900

    postman

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -5,7 +5,10 @@ var app = express();
 var port = 8000;
 app.get("/", function (req, res) {
     console.log(req);
-    res.send("Hello World!");
+    res.send({ hello: "world!" });
+});
+app.post("/test", function (req, res) {
+    res.send({ person: "Lee" });
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:" + port);
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -5,7 +5,11 @@ const port: number = 8000;
 app.get("/", (req: express.Request, res: express.Response) => {
   //경로가 없으면 헬로월드를 출력하라
   console.log(req);
-  res.send("Hello World!");
+  res.send({ hello: "world!" });
+});
+
+app.post("/test", (req, res) => {
+  res.send({ person: "Lee" });
 });
 
 app.listen(port, () => {
