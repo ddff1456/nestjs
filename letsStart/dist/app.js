611586dd7e07bfd4e4913b45f4cabcb8d4f69cb1
kyeongikLee <ddff1456@naver.com>
2022-09-05 00:07:16 +0900

    middle ware

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -4,9 +4,28 @@ var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
 var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is som middleware");
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    res.send({ error: "404 not found error" });
 });
 app.listen(8000, function () {
     console.log("server is on...");
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,9 +4,36 @@ const app: express.Express = express();
 
 const data = [1, 2, 3, 4];
 
+app.use((req, res, next) => {
+  //미들웨어 (next를 추가하게되면 미들웨어가 된다.)
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  //미들웨어
+  console.log(req.rawHeaders[1]);
+  console.log("this is som middleware");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  //미들웨어 (next를 추가하게되면 미들웨어가 된다.)
+  console.log(req.rawHeaders[1]);
+  res.send({ error: "404 not found error" });
 });
 
 app.listen(8000, () => {
