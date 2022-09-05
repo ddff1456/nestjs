e75c6c5cac93aba77a2453851bb66c9fbbbf10cc
kyeongikLee <ddff1456@naver.com>
2022-09-05 23:11:12 +0900

    singleton pattern

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,28 +1,49 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
-const app: express.Express = express();
+class Server {
+  public app: express.Application;
 
-const data = [1, 2, 3, 4];
-//* logging middleware
-app.use((req, res, next) => {
-  //미들웨어 (next를 추가하게되면 미들웨어가 된다.)
-  console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware");
-  next();
-});
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-//* json middleware
-app.use(express.json()); //json 데이터 형식을 쓰게 해주는 미들웨어
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-app.use(catsRouter);
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      //미들웨어 (next를 추가하게되면 미들웨어가 된다.)
+      console.log(req.rawHeaders[1]);
+      console.log("this is logging middleware");
+      next();
+    });
 
-// * 404 middleware
-app.use((req, res, next) => {
-  // 내가만든 get,post가 없으면 해당 미들웨어에 타서 404 에러를 보여준다.(제일 밑에 놔둔다.)
-  console.log(req.rawHeaders[1]);
-  res.send({ error: "404 not found error" });
-});
+    //* json middleware
+    this.app.use(express.json()); //json 데이터 형식을 쓰게 해주는 미들웨어
 
-app.listen(8000, () => {
-  console.log("server is on...");
-});
+    this.setRoute();
+
+    // * 404 middleware
+    this.app.use((req, res, next) => {
+      // 내가만든 get,post가 없으면 해당 미들웨어에 타서 404 에러를 보여준다.(제일 밑에 놔둔다.)
+      console.log(req.rawHeaders[1]);
+      res.send({ error: "404 not found error" });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log("server is on...");
+    });
+  }
+}
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
--- letsStart/src/cats/cats.route.ts
+++ letsStart/src/cats/cats.route.ts
@@ -1,136 +1,54 @@
 import { Cat, CatType } from "./cats.model";
 import { Router } from "express";
+import {
+  createCat,
+  deleteCat,
+  readAllcat,
+  readCat,
+  updateCat,
+  updateParialCat,
+} from "./cats.service";
 
 const router = Router();
+//싱글톤 패턴을 사용하는 이유 최초 한번의 new연산자통해서 객체를 만들수 있어서 추후 객체에 접근할때 메모리낭비를 방지할 수 있음. 다른 클래스간의 데이터 공유자 쉽다.
 
 //* Read 고양이 전체 데이터 다 조회
-router.get("/cats", (req, res) => {
-  try {
-    const cats = Cat;
-    //throw new Error("db connect error"); //일부러 400 에러내기
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+router.get("/cats", readAllcat);
 //* Read 특정 고양이 데이터 조회
-router.get("/cat/:id", (req, res) => {
-  // :을 붙이면 파라메타가 된다.
-  try {
-    const params = req.params;
-    console.log(params);
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+// router.get("/cat/:id", (req, res) => {
+//   // :을 붙이면 파라메타가 된다.
+//   try {
+//     const params = req.params;
+//     console.log(params);
+//     const cat = Cat.find((cat) => {
+//       return cat.id === params.id;
+//     });
+//     res.status(200).send({
+//       success: true,
+//       data: {
+//         cat,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(400).send({
+//       success: false,
+//       error: "error",
+//     });
+//   }
+// });
+
+router.get("/cats/:id", readCat);
 
 //* CREATE 새로운 고양이 추가
-router.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    Cat.push(data); //Create 한다는 가정
-    console.log(data);
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+router.post("/cats", createCat);
 
 //* UPDATE 고양이 데이터 업데이트 -> PUT
-router.put("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    console.log(params);
-    const body = req.body;
-    console.log(body);
-    let result;
-    Cat.forEach((cat) => {
-      if (cat.id == params.id) {
-        cat = body;
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+router.put("/cats/:id", updateCat);
+
 //* UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
-router.patch("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    console.log(params);
-    const body = req.body;
-    console.log(body);
-    let result;
-    Cat.forEach((cat) => {
-      if (cat.id == params.id) {
-        cat = { ...cat, ...body };
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+router.patch("/cats/:id", updateParialCat);
+
 //* DELETE 고양이 데이터 삭제 -> DELETE
-router.delete("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const newCat = Cat.filter((cat) => cat.id !== params.id);
-    res.status(200).send({
-      success: true,
-      data: newCat,
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: "error",
-    });
-  }
-});
+router.delete("/cats/:id", deleteCat);
 //반드시 지킬 필요는 없다.
 export default router;
